7fb28de319ed3b3b45f28aebc8f1e4747aa95092
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-03-12 09:18:41 -0300

    ainda não to entendendo porra nenhuma

--- ordenacao/mergesort/mergesort.c
+++ ordenacao/mergesort/mergesort.c
@@ -8,8 +8,8 @@ void merge(int vetor[], int comeco, int meio, int fim);
 int main(void)
 {
 
-  int SIZE = 15;
-  int dados[15] = {5,4,3,2,1,39,6,8,9,10,11,12,13,50,80};
+  int SIZE = 7;
+  int dados[7] = {0,5,4,3,2,1,9};
 
   // DEBUG
   for (int i = 0; i < SIZE; i++)
@@ -29,57 +29,56 @@ void mergeSort(int vetor[], int comeco, int fim)
 {
   if (comeco < fim)
   {
-    int meio = floor((comeco  + fim) / 2); // (0 + 9) / 2 = 4.5
+    int meio = floor((comeco  + fim) / 2);
+
     mergeSort(vetor, comeco, meio);
     mergeSort(vetor, meio + 1, fim);
+
     merge(vetor, comeco, meio, fim);
   }
 }
 
 void merge(int vetor[], int comeco, int meio, int fim)
 {
-  int comecoParte1 = comeco;
-  int fimParte1 = 0;
+  int com1 = comeco, 
+      com2 = meio+1, 
+      comAux = 0, 
+      tam = fim-comeco+1;
 
-  int comecoParte2 = meio + 1;
-  int fimParte2 = 0;
+  int *vetAux;
+  vetAux = (int*) malloc(tam * sizeof(int));
 
-  int tamanho = fim - comeco + 1;
-
-  int *temp; // vetor auxiliar temporário
-  temp = (int *) malloc(tamanho * sizeof(int));
-
-  if(temp != NULL){
-    for (int i = 0; i < tamanho; i++) {
-      if (!fimParte1 && !fimParte2) { // enquanto não chegar no final dos vetores 
-        // Faz a ordenação
-        if (vetor[comecoParte1] < vetor[comecoParte2]) {
-          temp[i] = vetor[comecoParte1];
-          comecoParte1++;
-        } else {
-          temp[i] = vetor[comecoParte2];
-          comecoParte2++;
-        }
-        // Verifica se chegou no fim das duas metades
-        if (comecoParte1 > meio) fim = 1;
-        if (comecoParte2 > fim) fimParte2 = 1;
-      } else {
-        if (!fimParte1) {
-          temp[i] = vetor[comecoParte1];
-          comecoParte1++;
-        } else {
-          temp[i] = vetor[comecoParte2];
-          comecoParte2++;
-        }
-      }
-    }
-  }
-
-  int j, k;
-  for (j = 0, k = comeco; j < tamanho; j++, k++)
+  while (com1 <= meio && com2 <= fim) 
   {
-    vetor[k] = temp[j];
+    if(vetor[com1] < vetor[com2]) {
+      vetAux[comAux] = vetor[com1];
+      com1++;
+    } else {
+      vetAux[comAux] = vetor[com2];
+      com2++;
+    }
+    comAux++;
+  }
+  
+
+  while (com1 <= meio) 
+  {
+    vetAux[comAux] = vetor[com1];
+    comAux++;
+    com1++;
   }
 
-  free(temp);
+  while (com2 <= fim) 
+  {
+    vetAux[comAux] = vetor[com2];
+    comAux++;
+    com2++;
+  }
+  
+    for(comAux = comeco; comAux <= fim; comAux++){    //Move os elementos de volta para o vetor original
+      vetor[comAux] = vetAux[comAux-comeco];
+  }
+  
+  free(vetAux);
+
 }
