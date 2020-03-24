b971b189a29f5b3ad762f453a52d9925232e7324
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-03-24 07:36:13 -0300

    adiciona quick sort

--- ordenacao/quicksort/quicksort.c
+++ ordenacao/quicksort/quicksort.c
@@ -1,5 +1,52 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+  int x = *a;
+  *a = *b;
+  *b = x;
+}
+
+int partition(int arr[], int low, int high)
+{
+  int pivot = arr[high];
+  int i = (low - 1);
+
+  for (int j = low; j <= high; j++)
+  {
+    if (arr[j] < pivot)
+    {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+  if (low < high)
+  {
+    int pivot = partition(arr, low, high);
+    quickSort(arr, low, pivot - 1);
+    quickSort(arr, pivot + 1, high);
+  }
+}
+
+void printArray(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
 void main(void)
 {
+  int arr[7] = {1, 7, 3, 2, 10, 0, 5};
+  quickSort(arr, 0, 6);
+
+  printArray(arr, 7);
 }
