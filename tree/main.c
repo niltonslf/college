5215560d6f977d8122e7382b100df14778fc6aed
Nilton Lopes <nilton.lopes@madeiramadeira.com.br>
2020-04-19 02:49:07 -0300

    Adiciona algoritmo de remoção

--- tree/main.c
+++ tree/main.c
@@ -8,16 +8,21 @@ int main()
     // Root test = createTree(createTree(createTree(NULL,4,NULL), 2, createTree(NULL,5,NULL)),1,createTree(NULL,3,createTree(NULL,6,NULL)));
 
     Tree *root = createTree();
-    insertNode(root, 3);
-
-    insertNode(root, 2);
-    insertNode(root, 5);
-    insertNode(root, 8);
-    insertNode(root, 10);
+    insertNode(root, 30);
     insertNode(root, 15);
-    insertNode(root, 1);
+    insertNode(root, 20);
+    insertNode(root, 7);
+    insertNode(root, 40);
+    insertNode(root, 60);
+    insertNode(root, 31);
+    insertNode(root, 35);
+    insertNode(root, 39);
 
-    printf("debug: %d\n", (*root)->left->left->item);
+    removeNode(root, 7);
+    removeNode(root, 40);
+
+    printf("DEBUG LEFT: %d \n", (*root)->right->left->item);
+    printf("DEBUG RIGHT: %d \n", (*root)->right->right->item);
 
     printf("\n FIM \n");
 
--- tree/tree.h
+++ tree/tree.h
@@ -20,15 +20,6 @@ Tree *createTree()
   return node;
 }
 
-// Tree createTree(Tree leftTree, Item item, Tree rightTree) {
-//     Tree node =  malloc(sizeof(Tree));
-//     node->left = leftTree;
-//     node->right = rightTree;
-//     node->item = item;
-
-//     return node;
-// }
-
 int insertNode(Tree *node, Item item)
 {
 
@@ -52,15 +43,80 @@ int insertNode(Tree *node, Item item)
 
   if (item < (*node)->item)
   { // inserir à esquerda
-
     printf("\nelemento: %d é menor que %d. inserindo à esquerda.\n", item, (*node)->item);
-
     insertNode(&(*node)->left, item);
   }
+
   else
   { // inserir à direita
     printf("\nelemento: %d é maior que %d. inserindo à direita.\n", item, (*node)->item);
-
     insertNode(&(*node)->right, item);
+  }
+}
+
+int removeNode(Tree *node, Item item)
+{
+  if (node == NULL)
+    return 0; // arvore não existe
+
+  if (*node == NULL) // àrvore está vaziaw
+    return 0;
+
+  // achou o elemento a ser removido?
+  if (item == (*node)->item)
+  {
+    printf("\nRemoção - elemento: %d encontrado.\n", item);
+
+    // -- é um nó folha?
+    if ((*node)->left == NULL && (*node)->right == NULL)
+    {
+      printf("\nRemoção - elemento: %d é um nó folha.\n", item);
+      *node = NULL;
+      return 1;
+    }
+    else if ((*node)->left != NULL && (*node)->right == NULL)
+    { // HÁ ELEMENTOS SOMENTE À ESQUERDA
+      printf("\nRemoção - elemento: %d é um possui 1 filho à esquerda.\n", item);
+
+      *node = (*node)->left;
+      return 1;
+    }
+    else if ((*node)->left == NULL && (*node)->right != NULL)
+    { // HÁ ELEMENTOS SOMENTE À DIREITA
+      printf("\nRemoção - elemento: %d é um possui 1 filho à direita.\n", item);
+
+      *node = (*node)->right;
+      return 1;
+    }
+    else
+    {
+      printf("\nRemoção - elemento: %d é um possui 2 filhos.\n", item);
+
+      Tree aux = (*node)->left; // pega o filho à esquerda
+      Tree auxPrev = (*node)->left;
+
+      while (aux->right != NULL) // pegar o ultimo elemento mais a direita
+      {
+        auxPrev = aux;
+        aux = aux->right;
+      }
+
+      auxPrev->right = NULL;       // remove apontamento do elemento anterior ao último elemento (aux)
+      aux->right = (*node)->right; // aux->right aponta para o filho a direita do elemento que será removido
+      aux->left = (*node)->left;   // aponta para o filho à esquerda do elemento que será removido
+      *node = aux;                 // remove elemento trocando o apontamento para o novo elemento
+      return 1;
+    }
+
+    // -- árvore com único nó
+  }
+
+  if (item < (*node)->item)
+  { // ESQUERDA
+    removeNode(&(*node)->left, item);
+  }
+  else
+  { // DIREITA
+    removeNode(&(*node)->right, item);
   }
 }
